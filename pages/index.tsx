ce9a9c49549320e0e8fe3d0d85fae168dbcb6a74
Rami <rami.alazzawe@outlook.com>
2025-01-09 20:13:31 -0500

    feat: about me and skills link added, etc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+
 import Navbar from "../components/NavBar";
 import { Reveal } from "../components/Reveal";
 import { RevealLTR } from "../components/RevealLTR";
@@ -56,7 +58,7 @@ export default function Home() {
         {/* Hero Section */}
         <section
           id="about"
-          className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-950 to-blue-700 px-[5vw] py-[5vh]"
+          className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-950 to-blue-700 px-[5vw] py-[5vh] relative"
         >
           <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
             {/* Left side: About Me */}
@@ -68,9 +70,34 @@ export default function Home() {
                 </h1>
               </RevealLTR>
               <RevealLTR delay={0.75}>
+
                 <p className="text-3xl text-gray-300 mb-8">
                   student | software engineer
+                  <div className="flex items-center mt-6">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+
+                  <Link
+                    href="#desc"
+                    className="text-gray-400 text-bold text-xl whitespace-nowrap mr-1"
+                  >
+                    {"About Me"}
+                  </Link>
+                  </div>
                 </p>
+
               </RevealLTR>
               {/* About Me Section */}
             </div>
@@ -88,6 +115,29 @@ export default function Home() {
               </div>
             </div>
           </div>
+          <div className="flex items-center absolute right-4 bottom-[100px]">
+            <Link
+              href="#skills"
+              className="text-white text-bold underline underline-offset-2 text-xl whitespace-nowrap mr-1"
+            >
+              {"Jump to skills"}
+            </Link>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="white"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+              />
+            </svg>
+          </div>
         </section>
 
         {/* Description */}
@@ -96,34 +146,35 @@ export default function Home() {
           {/* Left side: About Me */}
 
           <div className="w-1/2 text-left lg:mr-auto flex flex-col justify-left items-left ml-4 pl-4 pr-4 mb-10">
-            <h1 className="text-5xl font-bold mt-10 text-indigo-950 pt-10">
+            <h1 className="text-5xl font-bold mt-10 underline underline-offset-8 decoration-4 decoration-blue-500 text-indigo-950 pt-10 ">
               {"{ about me }"}
             </h1>
             <div className="flex-grow flex flex-col justify-evenly text-xl">
-              
               <div>
                 <h1 className="text-xl font-bold mt-10 text-indigo-950 pt-10">
                   {"{ who am i ?}"}
                 </h1>
-                <p className="text-gray-600 max-w-prose ">
-                  // My name is Rami Al-Azzawe, and I am in my third year of my
-                  Computer Science degree as a Software Engineering Specialist at the University of Toronto.
+                <p className="text-gray-600 max-w-prose text-lg">
+                  // My name is Rami Al-Azzawe. I am a third year Computer
+                  Science Specialist student at the University of Toronto.
                 </p>
               </div>
               <div>
-                <h1 className="text-xl font-bold mt-10 text-indigo-950 pt-10">
+                <h1 className="text-xl font-bold text-indigo-950 pt-10">
                   {"{ areas of interest }"}
                 </h1>
-                <p className="text-gray-600 max-w-prose">
-                  // As of right now, I am continuing to explore my interests. Some platforms
-                  that greatly interest me are web development and app development, and 
+                <p className="text-gray-600 max-w-prose text-lg">
+                  // I am continuing to explore my interests, and few that stand
+                  out to me today are full stack web development and mobile app
+                  development. Much of this experience is also complemented with
+                  my experience in data structures and OOP principles
                 </p>
               </div>
               <div>
-                <h1 className="text-xl font-bold mt-10 text-indigo-950 pt-10">
+                <h1 className="text-xl font-bold text-indigo-950 pt-10">
                   {"{ goals && aspirations }"}
                 </h1>
-                <p className="text-gray-600 max-w-prose">
+                <p className="text-gray-600 max-w-prose text-lg">
                   // I am a hardworking and dedicated individual, and these
                   traits have always been something I pride myself on as they
                   have always, and continue to, bring me success and ultimately
@@ -132,19 +183,17 @@ export default function Home() {
                 </p>
               </div>
               <div>
-                <h1 className="text-xl font-bold mt-10 text-indigo-950 pt-10">
+                <h1 className="text-xl font-bold text-indigo-950 pt-10">
                   {"{ hobbies && interests }"}
                 </h1>
-                <p className="text-gray-600 max-w-prose">
+                <p className="text-gray-600 max-w-prose text-lg">
                   // In addition to my strong passion and aptitude for anything
                   technology related, I am a very music oriented person. I have
-                  been playing the guitar since I was 10 years old. My genres
-                  range anywhere from pop, to throwbacks from decades as early
-                  as the 50s, where one day you'll find me listening to One
-                  Direction, and the next, the legend Billy Joel.
-                  <br/><br/> Lastly, fitness is 
-                  another very important part of my life, and is something I ensure to 
-                  fit into my schedule for physical and mental wellbeing
+                  played the guitar since 10 years old, and my genres range
+                  anywhere from pop, to throwbacks from decades as early as the
+                  50s. Along with this interest is my strong passion for
+                  fitness, which is something I ensure to fit into my schedule
+                  as an excellent way to maintain physical and mental wellbeing
                 </p>
               </div>
             </div>
