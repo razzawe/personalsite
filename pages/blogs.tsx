2ce30ce4c7025f5b06b84f847ffd190ec79eaf3a
Rami <rami.alazzawe@outlook.com>
2025-06-16 21:04:06 -0400

    feat: revamped block page

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <NavBar/>
         <section
         id="about"
-        className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-950 to-blue-700 px-[5vw] py-[5vh]"
+        className="min-h-screen flex items-center justify-center bg-gradient-to-b from-gray-900 to-gray-700 px-[5vw] py-[5vh]"
       >
         <h1 className="text-center items-center justify-center text-white text-bold text-xl">Coming Soon!</h1>
         </section>
