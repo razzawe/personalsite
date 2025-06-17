74a42d0f6cf782172b2e87d3d5c79116b3b6c52d
Rami <rami.alazzawe@outlook.com>
2025-06-16 21:04:41 -0400

    feat: revamped index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,19 +65,19 @@ export default function Home() {
         {/* Hero Section */}
         <section
           id="about"
-          className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-950 to-blue-700 px-[5vw] py-[5vh] relative"
+          className="min-h-screen flex items-center justify-center bg-gradient-to-b from-gray-900 to-gray-700 px-[5vw] py-[5vh] relative"
         >
           <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
             {/* Left side: About Me */}
 
             <div className="lg:w-1/2 text-left mb-8 lg:mb-0">
               <RevealLTR width="100%">
-                <h1 className="text-7xl font-bold text-white mb-4">
+                <h1 className="md:text-7xl text-5xl font-bold text-white mb-4">
                   Rami Al-Azzawe
                 </h1>
               </RevealLTR>
               <RevealLTR delay={0.75}>
-                <p className="text-3xl text-gray-300 mb-8">
+                <p className="md:text-3xl text-xl text-gray-300 mb-8">
                   student | software engineer
                 </p>
                 <div className="flex items-center mt-6">
@@ -123,7 +123,7 @@ export default function Home() {
           <div className="flex items-center absolute right-4 bottom-[100px]">
             <Link
               href="#skills"
-              className="text-white text-bold underline underline-offset-2 text-xl whitespace-nowrap mr-1"
+              className="text-white text-bold underline underline-offset-2 md:text-xl text-sm whitespace-nowrap mr-1"
             >
               {"Jump to skills"}
             </Link>
@@ -145,78 +145,64 @@ export default function Home() {
           </div>
         </section>
 
-        {/* Description */}
-
-        <section id="desc" className="flex min-h-screen bg-white ">
-          {/* Left side: About Me */}
-
-          <div className="w-1/2 text-left lg:mr-auto flex flex-col justify-left items-left ml-4 pl-4 pr-4 mb-10">
-            <h1 className="text-5xl font-bold mt-10 underline underline-offset-8 decoration-4 decoration-blue-500 text-indigo-950 pt-10 ">
-              {"{ about me }"}
-            </h1>
-            <div className="flex-grow flex flex-col justify-evenly text-xl">
-              <div>
-                <h1 className="text-xl font-bold mt-10 text-indigo-950 pt-10">
-                  {"{ who am i ?}"}
-                </h1>
-                <p className="text-gray-600 max-w-prose text-lg">
-                  // My name is Rami Al-Azzawe. I am a third year Computer
-                  Science Specialist student at the University of Toronto.
-                </p>
-              </div>
-              <div>
-                <h1 className="text-xl font-bold text-indigo-950 pt-10">
-                  {"{ areas of interest }"}
-                </h1>
-                <p className="text-gray-600 max-w-prose text-lg">
-                  // I am continuing to explore my interests, and few that stand
-                  out to me today are full stack web development and mobile app
-                  development. Much of this experience is also complemented with
-                  my experience in data structures and OOP principles
-                </p>
-              </div>
-              <div>
-                <h1 className="text-xl font-bold text-indigo-950 pt-10">
-                  {"{ goals && aspirations }"}
-                </h1>
-                <p className="text-gray-600 max-w-prose text-lg">
-                  // I am a hardworking and dedicated individual, and these
-                  traits have always been something I pride myself on as they
-                  have always, and continue to, bring me success and ultimately
-                  closer to reaching all of my career goals as an aspiring
-                  Software Engineer.
-                </p>
-              </div>
-              <div>
-                <h1 className="text-xl font-bold text-indigo-950 pt-10">
-                  {"{ hobbies && interests }"}
-                </h1>
-                <p className="text-gray-600 max-w-prose text-lg">
-                  // In addition to my strong passion and aptitude for anything
-                  technology related, I am a very music oriented person. I have
-                  played the guitar since 10 years old, and my genres range
-                  anywhere from pop, to throwbacks from decades as early as the
-                  50s. Along with this interest is my strong passion for
-                  fitness, which is something I ensure to fit into my schedule
-                  as an excellent way to maintain physical and mental wellbeing
-                </p>
-              </div>
-            </div>
-          </div>
-
-          {/* Right side: Image */}
-          <div className="w-1/2 flex justify-center items-center border-4 border-indigo-950 lg:ml-auto">
+        {/* <section id="desc" className="flex flex-row min-h-screen bg-gradient-to-b from-gray-700 to-gray-900">
+          
+        <div className="hidden md:block w-1/2 flex justify-center items-center ">
             <div className="relative w-full h-full">
               <Image
-                src="/img/mepic.jpg"
+                src="/img/IMG_0403.jpg"
                 alt="Profile picture"
                 fill
                 sizes="(max-width: 768px) 320px, 500px"
-                className="border-4 border-gray-700 shadow-lg object-cover"
+                className="shadow-lg object-cover"
               />
             </div>
           </div>
-        </section>
+
+        <div>
+          <p>hello</p>
+        </div>
+
+
+        </section> */}
+<section id="desc" className="grid grid-cols-1 md:grid-cols-2 min-h-screen bg-gradient-to-b from-gray-700 to-gray-900">
+  {/* Left Side: Image */}
+  <div className="hidden md:flex justify-center items-center p-4">
+    <div className="relative w-[400px] h-[600px] lg:w-[500px] lg:h-[700px]">
+      <Image
+        src="/img/IMG_0403.jpg"
+        alt="Profile picture"
+        fill
+        sizes="(max-width: 768px) 400px, 700px"
+        className="shadow-lg rounded-lg object-cover"
+      />
+    </div>
+  </div>
+
+  {/* Right Side: Text */}
+  <div className="flex flex-col justify-center items-start p-4 md:pl-6 text-white space-y-4">
+    <h2 className="text-4xl font-bold">{"{ about me }"}</h2>
+    <p className="text-lg text-gray-300">
+     I am Rami Al-Azzawe, a Software Engineer in the making, now studying at the University of Toronto, specializing in Computer Science.
+    </p>
+    <p className="text-lg text-gray-300">
+    I have a strong interest and aptitute for tech and how it shapes the world around us; and that is something which has been a staple of my life since I was young.
+    </p>
+    <p className="text-lg text-gray-300">
+      From a young age, I remember beginning my fascination with technology using a Windows XP laptop and developed a strong interest for Windows operating systems as a whole; which led to me diving into various operating systems through a VM such as Windows 95, 98, ME, and more (like any other kid that age would do... right?).
+    </p>
+    <p className="text-lg text-gray-300">
+      Thereon, over the years, I continued down the path of my passion for technology, developing some games in elementary school with pre-made assets, to creating a simple platformer video game from scratch based off the Mandalorian in high school, to today, where I am continuing to explore my interests in full stack web development and mobile app development.
+    </p>
+
+    <Link
+      href="#skills"
+      className="text-blue-400 font-bold underline underline-offset-4 hover:text-blue-300"
+    >
+      Learn more about my skills →
+    </Link>
+  </div>
+</section>
 
         {/* Technical Skills Section */}
 
@@ -225,13 +211,6 @@ export default function Home() {
             id="skills"
             className="py-20 px-4 sm:px-6 lg:px-8 relative text-white"
           >
-            {/* Background Blur */}
-            <div
-              className="absolute inset-0 bg-cover bg-center opacity-4 filter blur-md z-0 bg-gradient-to-tr"
-              style={{
-                backgroundImage: "url(/img/code.jpg)",
-              }}
-            ></div>
 
             <div className="max-w-4xl mx-auto relative z-10">
               <h2 className="text-3xl font-bold text-center text-white mb-12">
@@ -243,12 +222,12 @@ export default function Home() {
                 <h1 className="text-3xl font-bold">// languages</h1>
               </div>
 
-              <div className="grid grid-cols-4 gap-4 ml-10">
+              <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4 ml-10 break-words">
                 {languages.map((language, index) => (
-                  <div className="group items-center justify-center text-center font-bold bg-red-500 p-6 pt-10 pb-10 m-3 rounded-lg bg-gradient-to-tr from-blue-950 to-indigo-950 border-4 border-transparent transition-colors duration-300 hover:border-white ">
+                  <div className="group items-center justify-center text-center font-bold bg-red-500 p-6 pt-10 pb-10 m-3 rounded-lg bg-gradient-to-tr from-blue-500 to-indigo-400 border-4 border-transparent transition-colors duration-300 hover:border-white ">
                     {language.language}
                     <div className="flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                      <p className="text-gray-400 text-sm mt-2 ">Used in: {language.usage}</p>
+                      <p className="text-sm mt-2 ">Used in: {language.usage}</p>
                     </div>
                   </div>
                 ))}
@@ -258,17 +237,17 @@ export default function Home() {
                 // frameworks && developer tools
               </h1>
 
-              <div className="grid grid-cols-4 gap-4 ml-10">
+              <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4 ml-10 break-words">
                 {tools.map((tool, index) => (
-                  <div className="text-center font-bold bg-red-500 p-3 pt-5 pb-5 m-1 rounded-lg bg-gradient-to-tr from-blue-950 to-indigo-950 ">
+                  <div className="text-center font-bold bg-red-500 p-3 pt-5 pb-5 m-1 rounded-lg bg-gradient-to-tr from-blue-500 to-indigo-400 ">
                     {tool}
                   </div>
                 ))}
               </div>
               <h1 className="text-3xl font-bold">// additional skills</h1>
-              <div className="grid grid-cols-4 gap-4 ml-10">
+              <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4 ml-10 break-words">
                 {additional.map((skill, index) => (
-                  <div className="text-center font-bold bg-red-500 p-6 pt-3 pb-3 m-1 rounded-lg bg-gradient-to-tr from-blue-950 to-indigo-950 ">
+                  <div className="text-center font-bold bg-red-500 p-6 pt-3 pb-3 m-1 rounded-lg bg-gradient-to-tr from-blue-500 to-indigo-400 ">
                     {skill}
                   </div>
                 ))}
